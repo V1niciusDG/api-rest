403ddc0b6c216637f5912a9e1161dd8102ea7c7a
Vinicius Dalanora <vinicius.dalanora@waterservicestech.com>
2023-11-22 16:08:14 -0300

    validates

--- src/controllers/cidades/Create.ts
+++ src/controllers/cidades/Create.ts
@@ -1,32 +1,61 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
-
 
 interface ICidade {
     nome: string;
+    estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
     nome: yup.string().required().min(3),
-    estado: yup.string().required().min(2)
-})
+    estado: yup.string().required().min(2),
+});
 
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-    let validatedData : ICidade | undefined = undefined
-
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
     try {
-        validatedData = await bodyValidation.validate(req.body);
+        await bodyValidation.validate(req.body, { abortEarly: false });
+        return next();
     } catch (error) {
         const yupError = error as yup.ValidationError;
+        const errors: Record<string, string> = {};
 
-        return res.json({
-            errors: {
-                default: yupError.message,
-            }
-        })
+        yupError.inner.forEach((error) => {
+            if (error.path === undefined) return;
+            errors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
     }
+};
 
-    console.log(validatedData);
+interface IFilter {
+    filter?: string;
+}
 
-    return res.send('create');
+const queryValidation: yup.Schema<IFilter> = yup.object().shape({
+    filter: yup.string().required().min(3),
+});
+
+export const createQueryValidator: RequestHandler = async (req, res, next) => {
+    try {
+        await queryValidation.validate(req.query, { abortEarly: false });
+        return next();
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        const errors: Record<string, string> = {};
+
+        yupError.inner.forEach((error) => {
+            if (error.path === undefined) return;
+            errors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+    }
+};
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    console.log(req.body);
+
+    return res.send("Create");
 };
--- src/server/routes/path.ts
+++ src/server/routes/path.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import { CidadesController } from '../../controllers';
+import { Router } from "express";
+import { CidadesController } from "../../controllers";
 const router = Router();
 
-router.get('/', (req, res) => {
-    return res.send('Olá, dev!');
-  });
+router.get("/", (req, res) => {
+    return res.send("Olá, dev!");
+});
 
-router.post('/cidades', CidadesController.create)
-
-
-
+router.post(
+    "/cidades",
+    CidadesController.createBodyValidator,
+    CidadesController.createQueryValidator,
+    CidadesController.create
+);
 
 export { router };
-
-
